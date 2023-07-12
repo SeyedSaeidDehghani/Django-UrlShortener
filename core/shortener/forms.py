711aebe7d39123d7dbf83db968df3a83facf3c4f
Seyed Saeid Dehghani <63464677+SeyedSaeidDehghani@users.noreply.github.com>
2023-07-12 05:29:56 +0330

    create view and form.

---
+++ core/shortener/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Shortener
+
+
+class ShortenerForm(forms.ModelForm):
+    class Meta:
+        model = Shortener
+        fields = ('original_url',)
+
+    def clean_original_url(self):
+        original_url = self.cleaned_data['original_url']
+        return original_url
--- core/shortener/views.py
+++ core/shortener/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.views import generic, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponsePermanentRedirect
+from .forms import ShortenerForm
+from .models import Shortener
+
 
 # Create your views here.
+
+class ShortenerListView(LoginRequiredMixin, generic.ListView):
+    """
+    this is a class base view for List of Urls page
+    """
+    model = Shortener
+    paginate_by = 6
+
+    def get_queryset(self):
+        return Shortener.objects.filter(user=self.request.user)
+
+
+class ShortenerDetailView(LoginRequiredMixin, generic.DetailView):
+    """
+    this is a class base view for Detail of Url Page
+    """
+    model = Shortener
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+
+class ShortenerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Shortener
+    form_class = ShortenerForm
+    template_name = 'shortener/shortener_create.html'
+    success_url = reverse_lazy("shortener:detail")
+
+    def form_valid(self, form):
+        original_url = form.instance.original_url
+
+        if Shortener.objects.filter(original_url=original_url, user=self.request.user).exists():
+            form.add_error(None, 'This url is exist')
+            return super().form_invalid(form)
+
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ShortenerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Shortener
+    success_url = reverse_lazy("shortener:list")
+
+
+class ShortenerRedirectView(LoginRequiredMixin, View):
+    """
+    this is a class base view for Redirect url
+    """
+
+    def get(self, request, short_url, *args, **kwargs):
+        obj = get_object_or_404(Shortener, short_url=short_url)
+        return HttpResponsePermanentRedirect(obj.original_url)
