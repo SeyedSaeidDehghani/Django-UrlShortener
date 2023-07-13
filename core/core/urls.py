f5f437ebcb593c9c6f189df0ac27f4b53f94d9d4
Seyed Saeid Dehghani <63464677+SeyedSaeidDehghani@users.noreply.github.com>
2023-07-13 04:21:36 +0330

    change index url

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.shortcuts import reverse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url=reverse('shortener:list'))),
     path("shortener/", include("shortener.urls")),
     path("accounts/", include("accounts.urls")),
 ]
